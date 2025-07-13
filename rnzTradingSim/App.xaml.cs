c688c37701c2120ab046c5e2c33f55b2dc6d63c7
unknown <125713061+ruan-luidy@users.noreply.github.com>
2025-07-12 21:42:52 -0300

    Initial commit

---
+++ rnzTradingSim/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace rnzTradingSim
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
